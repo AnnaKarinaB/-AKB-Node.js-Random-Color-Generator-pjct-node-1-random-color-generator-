daceb0ab96f79d9b1041edb8b2dc2162480391f8
Anna-Karina <annakarina.bernhart@gmail.com>
2022-05-03 19:34:09 +0200

    Add preflight changes

---
+++ index.js
@@ -0,0 +1,28 @@
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const rcolor = randomColor();
+
+const myBox = `
+###############################
+###############################
+###############################
+#####                     #####
+#####       ${rcolor}       #####
+#####                     #####
+###############################
+###############################
+###############################`;
+
+if (process.argv.length < 3) {
+  console.log(chalk.hex(rcolor)(myBox));
+} else {
+  console.log(
+    chalk.hex(
+      randomColor({
+        luminosity: process.argv[3],
+        hue: process.argv[2],
+      }),
+    )(myBox),
+  );
+}
